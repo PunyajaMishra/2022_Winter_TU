5d72ff49420d7724cd173c77ea03ff03fdc170b1
Punyaja Mishra <59839463+PunyajaMishra@users.noreply.github.com>
2023-07-05 15:48:33 -0400

    Add files via upload

---
+++ COIS 1400H/midterm.py
@@ -0,0 +1,20 @@
+
+for num in range(4):
+    print(num)
+
+#for num in range(2,9,2):
+ #   print(num)
+
+#for num in range(0,20,5):
+    #num+=num
+#print(num)
+
+#student = 1
+#while student <=3:
+    #total=0
+    #for score in range(1,4):
+    #   score = int(input("score: "))
+     #   total +=score
+    #average = total/3
+    #print("student", student, "average", average)
+    #student+=1
